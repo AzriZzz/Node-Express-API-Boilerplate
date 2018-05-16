f7a391f3eea0d06ca8f5d6dbc6fa2778ec811502
Muhammad Azri <lenovorosak@gmail.com>
2018-05-16 13:56:57 +0800

    fix error not found

--- routes/programe.js
+++ routes/programe.js
@@ -3,23 +3,23 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 // our db model
-var Programe = require("../models/programe.js");
+// var Programe = require("../models/programe.js");
 
 /**
  * GET '/programe'
- * Just relays a success message back.
+ * Programe home route. Just relays a success message back.
  * @param  {Object} req
  * @return {Object} json
  */
 router.get('/', function(req, res) {
-  
-  var jsonData = {
-  	'name': 'Programe',
-  	'api-status':'OK'
-  }
 
-  // respond with json data
-  res.json(jsonData)
-});
+    var jsonData = {
+        'name': 'Program',
+        'api-status':'OK'
+    }
+  
+    // respond with json data
+    res.json(jsonData)
+  });
 
 module.exports = router;
