4ca8f0c46c8b28f9bb8cd8109de2972f0aaafd5f
Muhammad Azri <lenovorosak@gmail.com>
2018-05-16 14:19:48 +0800

    add model, add object in cloud

---
+++ models/programe.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// See http://mongoosejs.com/docs/schematypes.html
+
+var programeScheme = new Schema({
+	id: {
+		type: String,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	prog_name: String,
+	cust_name: String,
+	date: { 
+		type: Date, 
+		default: Date.now 
+	},
+	redemption_no: String,
+	prod_item: String,
+	consignment_no: String,
+	courier_type: String,
+		
+})
+
+// export 'Programe' model so we can interact with it in other files
+module.exports = mongoose.model('Programe',programeScheme);
+
